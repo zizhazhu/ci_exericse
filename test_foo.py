198bef9fb02c95d43de6d2daf8adec26b691c548
zizhazhu <zizhazhu@gmail.com>
2017-11-14 15:04:43 +0800

    add new testcase

--- test_foo.py
+++ test_foo.py
@@ -10,6 +10,9 @@ class SimpleTest(unittest.TestCase):
     def test2(self):
         self.assertEqual(foo.divide(0, 1), 0)
 
+    def test3(self):
+        self.assertEqual(foo.divide(3, 3), 0)
+
 
 if __name__ == '__main__':
     unittest.main()
