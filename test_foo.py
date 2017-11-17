a43e2ee9860abb9aeafe36becc6aef67cc83eeaa
zizhazhu <zizhazhu@gmail.com>
2017-11-17 14:49:54 +0800

    add error log

--- test_foo.py
+++ test_foo.py
@@ -5,13 +5,13 @@ import foo
 class SimpleTest(unittest.TestCase):
 
     def test1(self):
-        self.assertEqual(foo.divide(2, 2), 1)
+        self.assertEqual(foo.divide(2, 2), 1, "2 / 2 = 1 failed")
 
     def test2(self):
-        self.assertEqual(foo.divide(0, 1), 0)
+        self.assertEqual(foo.divide(0, 1), 0, "0 / 1 = 0 failed")
 
     def test3(self):
-        self.assertEqual(foo.divide(3, 3), 0)
+        self.assertEqual(foo.divide(3, 3), 1, "3 / 3 = 1 failed")
 
 
 if __name__ == '__main__':
